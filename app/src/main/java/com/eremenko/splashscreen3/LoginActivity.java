d6d3bb70cf37e3dd5df0f8b0b3948ceb7f26b5b1
Liubov Eremenko <eremenko.luba79@gmail.com>
2021-04-11 19:25:18 +0100

    4 screens

---
+++ app/src/main/java/com/eremenko/splashscreen3/LoginActivity.java
@@ -0,0 +1,17 @@
+package com.eremenko.splashscreen3;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.RelativeLayout;
+
+import java.util.logging.Handler;
+
+public class LoginActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.sign_in);
+    }
+
+}
---
+++ app/src/main/java/com/eremenko/splashscreen3/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.eremenko.splashscreen3
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    Handler handler ;
+    Runnable runnable;
+    ImageView image;
+
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+       image = findViewById(R.id.id_logo);
+        image.animate().alpha(4000).setDuration(0);
+
+        handler = new Handler();
+        handler.postdelayed(new Runnable(){
+            @Override
+            public void run() {
+                Intent dsp = new Intent(MainActivity,this,LoginActivity.class);
+                startActivity(dsp);
+                finish();
+            }
+
+        },delay )
+
+
+
+    }
+}
