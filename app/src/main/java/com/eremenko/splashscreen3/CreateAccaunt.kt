5ac1cb4effe62c1c4b0c6e89413b517581580dcf
LubaEremenko <eremenko.luba79@gmail.com>
2021-05-05 01:00:40 +0100

    shared preference

--- app/src/main/java/com/eremenko/splashscreen3/CreateAccaunt.kt
+++ app/src/main/java/com/eremenko/splashscreen3/CreateAccaunt.kt
@@ -1,35 +1,102 @@
 package com.eremenko.splashscreen3
 
+import android.content.Context
+import android.content.Intent
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.util.Log
+import android.view.Gravity
+import android.view.View
+import android.widget.ShareActionProvider
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 import kotlinx.android.synthetic.main.activity_create__accaunt.*
 
 
 class CreateAccaunt : AppCompatActivity() {
+
+    //lateinit var sharedPreferences: SharedPreferences
+
+    // var isRemembered = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create__accaunt)
 
+        println("dlf")
 
-        this.id_btnCreateNewAccount.setOnClickListener{
-            val email = id_email.text.toString()
-            val pswd = id_password.text.toString()
-            val firstName = id_firstname.text.toString()
-            val lastName = id_lastname.text.toString()
+        id_btnCreateNewAccount.setOnClickListener {
+            println("dlf")
+            Log.d("CreateAccaunt", "Password is:")
 
-            print("Email is:  + email")
+
+
+
+/*
+
+            val intent = Intent(this, StationDetail::class.java)
+        startActivity(intent)
+*/
+
+
+        /*sharedPreferences = getSharedPreferences("SHARED_PREFS", Context.MODE_PRIVATE)
+
+           isRemembered = sharedPreferences.getBoolean("CHECKBOX", false)
+
+           if (isRemembered) {
+
+               val intent = Intent(this, StationDetail::class.java)
+               startActivity(intent)
+               finish()
+           }*/
+
+
+       // loadData()
+
+
+
+
+           // saveData()
+
+
+            /*val email: String =  id_email.text.toString()
+            val pswd: String = id_password.text.toString()
+            val firstName: String = id_firstname.text.toString()
+            val lastName: String  = id_lastname.text.toString()
+
+            val checkBox: Boolean = checkBox.isChecked
+
+            val editor: SharedPreferences.Editor = sharedPreferences.edit()
+            editor.putString("EMAIL", email)
+            editor.putString("PASSWORD", pswd)
+            editor.putString("FIRST_NAME", firstName)
+            editor.putString("LAST_NAME", lastName)
+            editor.putBoolean("CHECKBOX", checkBox)
+            editor.apply()
+
+            Toast.makeText(this, "Information saved!!!", Toast.LENGTH_LONG).show()
+
+            val intent = Intent(this, StationDetail::class.java)
+            startActivity(intent)
+            finish()*/
+
+
+            /* print("Email is:  + email")*/
 
             //did not show in logcat. WHy?
-            Log.d("CreateAccaunt", "Email is: " + email)
+            /*   Log.d("CreateAccaunt", "Email is: " + email)
             Log.d("CreateAccaunt", "Password is: $pswd")
             Log.d("CreateAccaunt", "First name is: " + firstName)
-            Log.d("CreateAccaunt", "Last name is: " + lastName)
+            Log.d("CreateAccaunt", "Last name is: " + lastName)*/
+
+           //onSave()
 
 
 
         }
+
+
 
         val actionBar = supportActionBar
         if (actionBar != null) {
@@ -42,4 +109,63 @@ class CreateAccaunt : AppCompatActivity() {
 
 
     }
-}
+
+   /* private fun saveData() {
+        val email: String = id_email.text.toString()
+        val pswd: String = id_password.text.toString()
+        val firstName: String = id_firstname.text.toString()
+        val lastName: String = id_lastname.text.toString()
+
+        val sharedPreferences: SharedPreferences = getSharedPreferences("sharedPrefs", Context.MODE_PRIVATE)
+        val editor: SharedPreferences.Editor = sharedPreferences.edit()
+        editor.apply {
+            putString("STRING_KEY", email)
+            putString("STRING_KEY", pswd)
+            putString("STRING_KEY", firstName)
+            putString("STRING_KEY", lastName)
+
+            putBoolean("BOOLEAN_KEY", checkBox.isChecked)
+
+
+        }.apply()
+
+        //made toast
+        val toast = Toast.makeText(this, "Saved successfully", Toast.LENGTH_LONG)
+        toast.setGravity(Gravity.TOP, 0, 140)
+        toast.show()
+
+    }
+
+
+    private fun loadData() {
+        val sharedPreferences: SharedPreferences = getSharedPreferences("sharedPrefs", Context.MODE_PRIVATE)
+        val savedString: String? = sharedPreferences.getString("STRING_KEY", null)
+        val savedBoolean: Boolean = sharedPreferences.getBoolean("BOOLEAN_KEY", false)
+
+        checkBox.isChecked = savedBoolean*/
+
+
+ /*fun onSave() {
+  //create  shared pref file
+  val pref = getPreferences(Context.MODE_PRIVATE)
+  val editor = pref.edit()
+
+  //save email
+  editor.putString("EMAIL" , id_email.text.toString())
+  //save pswd
+  editor.putString("PASSWORD" , id_password.text.toString())
+  //save first name
+  editor.putString("FIRST NAME" , id_firstname.text.toString())
+  //save first name
+  editor.putString("LAST NAME" , id_lastname.text.toString())
+  //Commit changes
+  editor.commit()
+
+  //made toast
+  val toast = Toast.makeText(applicationContext, "Saved successfully", Toast.LENGTH_LONG)
+  toast.setGravity(Gravity.TOP, 0, 140)
+  toast.show()
+*/
+
+
+    }
--- app/src/main/java/com/eremenko/splashscreen3/MainActivity.kt
+++ app/src/main/java/com/eremenko/splashscreen3/MainActivity.kt
@@ -3,23 +3,74 @@
 
 package com.eremenko.splashscreen3
 
+import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
 import android.os.Bundle
+import android.util.Log
+import android.view.Gravity
 import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_create__accaunt.*
 import kotlinx.android.synthetic.main.activity_sign_in.*
 import kotlinx.android.synthetic.main.activity_sign_in.*
+import kotlinx.android.synthetic.main.activity_sign_in.id_email
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var sharedPreferences: SharedPreferences
+
+   var isRemembered = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create__accaunt)
 
+        sharedPreferences = getSharedPreferences("SHARED_PREFS", Context.MODE_PRIVATE)
+
+        isRemembered = sharedPreferences.getBoolean("CHECKBOX", false)
+
+        if (!isRemembered) {
+
+            println(sharedPreferences.getString("EMAIL", "DEFAULT"))
 
 
-    }
+            val intent = Intent(this, StationDetail::class.java)
+            startActivity(intent)
+            finish()
+        }
+
+        id_btnCreateNewAccount.setOnClickListener {
+
+
+
+            val email: String =  id_email.text.toString()
+            val pswd: String = id_password.text.toString()
+            val firstName: String = id_firstname.text.toString()
+            val lastName: String  = id_lastname.text.toString()
+
+            val checkBox: Boolean = checkBox.isChecked
+
+            val editor: SharedPreferences.Editor = sharedPreferences.edit()
+            editor.putString("EMAIL", email)
+            editor.putString("PASSWORD", pswd)
+            editor.putString("FIRST_NAME", firstName)
+            editor.putString("LAST_NAME", lastName)
+            editor.putBoolean("CHECKBOX", checkBox)
+            editor.apply()
+
+            val toast = Toast.makeText(this, "Saved successfully", Toast.LENGTH_LONG)
+            toast.setGravity(Gravity.TOP, 0, 140)
+            toast.show()
+
+            println(sharedPreferences.getString("EMAIL", "DEFAULT"))
+
+        }
+
+
+
+        }
 
 
 
